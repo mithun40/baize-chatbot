7739e029797bb72bbd79e2805eaec734715145cd
Mithun Majumdar <mithunmajumdar1996@gmail.com>
2023-04-07 14:22:57 +0000

    api is building

---
+++ demo/appApi.py
@@ -0,0 +1,55 @@
+import os
+import logging
+import sys
+import torch
+from app_modules.utils import *
+from app_modules.presets import *
+from app_modules.overwrites import *
+
+from flask import Flask, request, jsonify
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s [%(levelname)s] [%(filename)s:%(lineno)d] %(message)s",
+)
+
+# base_model = sys.argv[1]
+base_model="decapoda-research/llama-7b-hf"
+lora_model="project-baize/baize-lora-7B"
+adapter_model = "project-baize/baize-lora-7B"
+tokenizer, model, device = load_tokenizer_and_model(base_model, adapter_model)
+
+app = Flask(__name__)
+
+@app.route('/predict', methods=['POST'])
+def api_predict():
+    data = request.json
+    text = data.get("text", "")
+    if not text:
+        return jsonify({"error": "Text not provided"}), 400
+
+    # Set your parameters as needed
+    top_p = 0.95
+    temperature = 1
+    max_length_tokens = 512
+    max_context_length_tokens = 2048
+    history = []
+
+    # Get the chatbot response
+    response = ""
+    for chatbot, h, status in predict(
+        text,
+        [],
+        history,
+        top_p,
+        temperature,
+        max_length_tokens,
+        max_context_length_tokens,
+    ):
+        history = h
+        response = status
+
+    return jsonify({"response": response})
+
+if __name__ == "__main__":
+    app.run(debug=True)
